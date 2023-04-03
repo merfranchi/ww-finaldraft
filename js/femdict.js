b3beb05223bb4ca722f8bda7cd9604fc9f4a7147
merfranchi <122292229+merfranchi@users.noreply.github.com>
2023-04-03 10:57:37 -0400

    Add files via upload

---
+++ js/femdict.js
@@ -0,0 +1,17 @@
+const grid = document.querySelector("#grid-shuffle");
+const items = gsap.utils.toArray(".defcard");
+
+document.querySelector("#shuffle").addEventListener("click", () => {
+
+    // Get the state
+  const state = Flip.getState(items);
+
+  // Do the actual shuffling
+  for(let i = items.length; i >= 0; i--) {
+      grid.appendChild(grid.children[Math.random() * i | 0]);
+  }
+
+  const footer = document.getElementsByClassName("footer");
+  footer.style.position = "fixed";
+
+});
